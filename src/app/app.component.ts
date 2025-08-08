07435872821fc4b22af5afc01176e94da64fdff8
wesleyfelix <wesley.felixdelima96@hotmail.com>
2025-08-08 17:18:33 -0300

    inclusão do angular material e criação de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule,MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
