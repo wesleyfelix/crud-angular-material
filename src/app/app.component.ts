9dd71f838f1227135289f2a81e4bdcc32d796d3e
wesleyfelix <wesley.felixdelima96@hotmail.com>
2025-08-08 17:27:40 -0300

    incajustando as rotas no toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule,MatToolbarModule],
+  imports: [RouterOutlet, RouterLink, MatButtonModule,MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
