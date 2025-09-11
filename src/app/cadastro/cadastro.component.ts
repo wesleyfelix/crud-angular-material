c81430cacfac40d4a8d0b380260993022cd580ab
wesleyfelix <wesley.felixdelima96@hotmail.com>
2025-09-10 21:14:35 -0300

    iniciando o cadastro de cliente

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { Cliente} from './cliente'
+
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  cliente: Cliente = Cliente.newCliente();
+  salvar(){
+    console.time('CadastroComponent');
+    console.log("Dados cliente: ", this.cliente)
+    console.timeEnd('CadastroComponent');
+  }
 
 }
---
+++ src/app/cadastro/cliente.ts
@@ -0,0 +1,15 @@
+import { v4 as uuid} from 'uuid';
+
+export class Cliente{
+  id?: string;
+  nome?: string;
+  cpf?: string;
+  dataNascimento?: string;
+  email?: string;
+
+  static newCliente(){
+      const cliente = new Cliente()
+      cliente.id = uuid();
+      return cliente;
+  }
+}
